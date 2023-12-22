cc2fc8fa72c223cc8311ece1081cc4a735691039
inyourface34456 <62214409+inyourface34456@users.noreply.github.com>
2023-12-22 19:56:57 +0000

    did some more work on sha1sum

--- coreutil-source/sha1sum/src/main.rs
+++ coreutil-source/sha1sum/src/main.rs
@@ -1,54 +1,54 @@
-use data_encoding::HEXUPPER;
 use clap::Parser;
+use data_encoding::HEXLOWER;
 use ring::digest::{Context, Digest, SHA1_FOR_LEGACY_USE_ONLY};
-use std::io::Read;
 use std::fs::File;
+use std::io::Read;
 use std::process::exit;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about)]
 struct Cli {
-  name: Vec<String>,
+    name: Vec<String>,
 
-  /// read in binary mode
-  #[arg(long="binary", short='b', default_value_t = false)]
-  binary: bool,
+    /// read in binary mode
+    #[arg(long = "binary", short = 'b', default_value_t = false)]
+    binary: bool,
 
-  /// read checksums from the FILEs and check them
-  #[arg(long, short='c' ,default_value_t = String::new())]
-  check: String,
+    /// read checksums from the FILEs and check them
+    #[arg(long, short='c' ,default_value_t = String::new())]
+    check: String,
 
-  /// create a BSD-style checksum
-  #[arg(long, default_value_t = false)]
-  tag: bool,
+    /// create a BSD-style checksum
+    #[arg(long, default_value_t = false)]
+    tag: bool,
 
-  /// read in text mode (default)
-  #[arg(long, short='t', default_value_t = true)]
-  text: bool,
+    /// read in text mode (default)
+    #[arg(long, short = 't', default_value_t = true)]
+    text: bool,
 
-  /// end each output line with NUL, not newline, and disable file name escaping
-  #[arg(long, short='z', default_value_t = false)]
-  zero: bool,
+    /// end each output line with NUL, not newline, and disable file name escaping
+    #[arg(long, short = 'z', default_value_t = false)]
+    zero: bool,
 
-  /// don't fail or report status for missing files
-  #[arg(long="ignore-missing", default_value_t = false)]
-  ignore_missing: bool,
+    /// don't fail or report status for missing files
+    #[arg(long = "ignore-missing", default_value_t = false)]
+    ignore_missing: bool,
 
-  /// don't print OK for each successfully verified file
-  #[arg(long, default_value_t = false)]
-  quiet: bool,
+    /// don't print OK for each successfully verified file
+    #[arg(long, default_value_t = false)]
+    quiet: bool,
 
-  /// don't output anything, status code shows success
-  #[arg(long, default_value_t = false)]
-  status: bool,
+    /// don't output anything, status code shows success
+    #[arg(long, default_value_t = false)]
+    status: bool,
 
-  /// exit non-zero for improperly formatted checksum lines
-  #[arg(long, default_value_t = false)]
-  strict: bool,
+    /// exit non-zero for improperly formatted checksum lines
+    #[arg(long, default_value_t = false)]
+    strict: bool,
 
-  /// warn about improperly formatted checksum lines
-  #[arg(long, short='w' ,default_value_t = false)]
-  warn: bool
+    /// warn about improperly formatted checksum lines
+    #[arg(long, short = 'w', default_value_t = false)]
+    warn: bool,
 }
 
 fn sha1_digest<R: Read>(mut reader: R) -> Result<Digest, std::io::Error> {
@@ -57,7 +57,7 @@ fn sha1_digest<R: Read>(mut reader: R) -> Result<Digest, std::io::Error> {
 
     loop {
         let count = reader.read(&mut buffer)?;
-      
+
         if count == 0 {
             break;
         }
@@ -67,41 +67,68 @@ fn sha1_digest<R: Read>(mut reader: R) -> Result<Digest, std::io::Error> {
     Ok(context.finish())
 }
 
-
-fn main() {
-    let cli = Cli::parse();
+fn output_hashes(cli: Cli) {
     let mut seperator = "  ";
 
     if cli.binary {
-      seperator = " *"
+        seperator = " *"
     }
-  
+
     for i in cli.name {
-      let data = match File::open(&i) {
-        Ok(dat) => dat,
-        Err(e) => {
-          if cli.strict {
-            println!("sha1sum: {}: {}", &i, e.to_string());
-            exit(1)
-          } else {
-            println!("sha1sum: {}: {}", &i, e.to_string());
-            continue;
-          }
-        }
-      };
-      let digest = match sha1_digest(data) {
-          Ok(dat) => dat,
-          Err(e) => {
-            if cli.strict {
-              println!("sha1sum: {}: {}", &i, e.to_string());
-              exit(1)
-            } else {
-              println!("sha1sum: {}: {}", &i, e.to_string());
-              continue;
+        let data = match File::open(&i) {
+            Ok(dat) => dat,
+            Err(e) => {
+                if cli.strict {
+                    println!("sha1sum: {}: {}", &i, e.to_string());
+                    exit(1)
+                } else {
+                    println!("sha1sum: {}: {}", &i, e.to_string());
+                    continue;
+                }
             }
-          }
         };
-      
-      println!("{}{}{}", HEXUPPER.encode(digest.as_ref()), seperator, &i);
+        let digest = match sha1_digest(data) {
+            Ok(dat) => dat,
+            Err(e) => {
+                if cli.strict {
+                    println!("sha1sum: {}: {}", &i, e.to_string());
+                    exit(1)
+                } else {
+                    println!("sha1sum: {}: {}", &i, e.to_string());
+                    continue;
+                }
+            }
+        };
+
+        println!("{}{}{}", HEXLOWER.encode(digest.as_ref()), seperator, &i);
+    }
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    if cli.check.is_empty() {
+        output_hashes(cli)
+    } else {
+        let mut sums_to_check = File::open(&cli.check)
+            .expect(format!("sha1sum: {}: could not open file", &cli.check).as_ref());
+        let mut data = String::new();
+        let _ = sums_to_check
+            .read_to_string(&mut data)
+            .expect(format!("sha1sum: {}: could not read file", &cli.check).as_ref());
+        let hashes: Vec<String> = data
+            .split('\n')
+            .map(|line| {
+                let line: Vec<String> =
+                    line.to_string().split(' ').map(|x| x.to_string()).collect();
+                if line[0].len() == 40 {
+                    return line[0].to_string();
+                } else {
+                    return "".to_string();
+                }
+            })
+            .collect();
+
+        println!("{:?}", hashes);
     }
 }
--- coreutil-source/yes/src/main.rs
+++ coreutil-source/yes/src/main.rs
@@ -3,22 +3,17 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(author, version, about, long_about)]
 struct Cli {
-    string: Option<String>
+    string: Option<String>,
 }
 
 fn main() {
     let cli = Cli::parse();
     match cli.string {
-      Some(dat) => {
-        loop {
-          println!("{dat}")
-        }
-      }
-      None => {
-        loop {
-          println!("yes")
-        }
-      }
+        Some(dat) => loop {
+            println!("{dat}")
+        },
+        None => loop {
+            println!("yes")
+        },
     }
-  
 }
