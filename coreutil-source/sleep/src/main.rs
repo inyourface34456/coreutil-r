259f42c34d269e4d06d9796bcd0154f67b9797a9
inyourface34456 <62214409+inyourface34456@users.noreply.github.com>
2023-12-23 18:48:31 +0000

    finished sleep

--- coreutil-source/sleep/src/main.rs
+++ coreutil-source/sleep/src/main.rs
@@ -1,3 +1,30 @@
+use std::thread::sleep;
+use std::time::Duration;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about)]
+struct Cli {
+    name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+  let cli = Cli::parse();
+
+  if cli.name.ends_with('s') {
+    let duration = cli.name.replace('s', "").parse().expect("not a number");
+    sleep(Duration::from_secs_f64(duration))
+  } else if cli.name.ends_with('m') {
+    let duration = cli.name.replace('m', "").parse().expect("not a number");
+    sleep(Duration::from_secs_f64(duration)*60)
+  } else if cli.name.ends_with('h') {
+    let duration = cli.name.replace('h', "").parse().expect("not a number");
+    sleep(Duration::from_secs_f64(duration)*3600)
+  } else if cli.name.ends_with('d') {
+    let duration = cli.name.replace('d', "").parse().expect("not a number");
+    sleep(Duration::from_secs_f64(duration)*216000)
+  } else {
+    let duration = cli.name.parse().expect("not a number");
+    sleep(Duration::from_secs_f64(duration))
+  }
 }
